41fab5e60162e05f1e5a988b1e1dfddb3443142a
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2024-11-09 13:55:46 -0300

    9/11

---
+++ DistribuidoraMayorista/Web/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Models;
+using Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : Controller
+    {
+        private readonly UserService _userService;
+        public RegisterController(UserService userService)
+        {
+            _userService = userService;
+        }
+    
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post(UserDto userDto)
+        {
+            return Ok(_userService.AddUser(userDto));
+        }
+    }
+}
